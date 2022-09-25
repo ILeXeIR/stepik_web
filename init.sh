a66199bc85814b3140fadf011c21cfbd4121da92
ILeXeIR <al.v.petukhov@gmail.com>
2022-09-25 19:21:56 +0400

    Add django project ASK

--- init.sh
+++ init.sh
@@ -1,4 +1,12 @@
+sudo apt-get update
+sudo apt-get full-upgrade -y python3
+sudo pip3 install --upgrade pip
+sudo pip3 install --upgrade django
+sudo pip3 install --upgrade gunicorn
+
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 cd /home/box/web
-sudo gunicorn -b 0.0.0.0:8080 -w 4 hello:app
+sudo gunicorn -b 0.0.0.0:8080 -w 4 hello:app &
+cd /home/box/web/ask
+sudo gunicorn -b 0.0.0.0:8000 -w 4 ask.wsgi:application &
